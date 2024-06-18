e9fc4b6060370a8848d99628fb67e9a02a669dff
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2024-06-18 10:25:10 +0100

    add layout.html; add section unit test

--- test/_unit/sections/data-sections.spec.js
+++ test/_unit/sections/data-sections.spec.js
@@ -1,3 +1,12 @@
 const summarySections = require('../../../apps/ecs/sections/summary-data-sections.js');
 const pages = require('../../../apps/ecs/translations/src/en/pages.json');
 
+describe('Apply Data Sections in summary and submission page', () => {
+  describe('Summary page data sections and Pages', () => {
+    it('should have summary page data sections and page translations that correlate', () => {
+      const sectionsKeys = Object.keys(summarySections).sort();
+      const pagesSectionsKeys = Object.keys(pages.confirm.sections).sort();
+      expect(pagesSectionsKeys).to.include.members(sectionsKeys);
+    });
+  });
+});
