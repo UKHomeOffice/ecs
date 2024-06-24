d1879a37d42b7c4c19832e1325b0e4d23dfffe6d
Vinodha Samiyappan <vinodha.samiyappan@digital.homeoffice.gov.uk>
2024-06-25 00:29:46 +0100

    moved the validation function outside to make it more generic

--- apps/ecs/behaviours/check-validation.js
+++ apps/ecs/behaviours/check-validation.js
@@ -4,14 +4,16 @@ const PRETTY_DATE_FORMAT = 'DD MMMM YYYY';
 
 module.exports = superclass => class extends superclass {
   validateField(key, req) {
+    const validationErrorFunc = (type, args) => new this.ValidationError(key, {type: type, arguments: [args]});
+
     if(key === 'tupe-date') {
-      const validationErrorFunc = (type, args) => new this.ValidationError(key, {type: type, arguments: [args]});
       const DateToBeValidated = req.form.values[key];
       const startDateOfWork = req.sessionModel.get('start-work-date');
       if(!validators.after(DateToBeValidated, startDateOfWork)) {
         return validationErrorFunc('after', [moment(startDateOfWork).format(PRETTY_DATE_FORMAT)]);
       }
     }
+
     return super.validateField(key, req);
   }
 };
